717ee39cd473f9c083de1f83304792360e5affef
lui viton <artem_5511@mail.ru>
2022-10-07 00:56:14 +0300

    04

---
+++ src/07Associativmassiv/07Associativmassiv.tsx
@@ -0,0 +1,26 @@
+
+
+let appartment = {
+    floor: 4,
+    door: 'metall',
+    kitchen: {
+        refregerator: 'LG',
+        furniture1: 'sofa',
+        furniture2: 'table',
+        electronic: {
+            tv: 'samsung',
+            lightspot: 5,
+            ligtonoff: true,
+        },
+        food: 'chiken',
+    },
+    room: 2,
+    chairs: 6,
+    people: {
+        name: 'artem',
+        age: 33
+    }
+}
+
+appartment.kitchen.electronic.tv
+appartment['people']['age']
